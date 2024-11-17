18ff25dfd4b3dda8d5f801d8ec2d54addba518bf
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-18 04:05:17 +0500

    added script start to run server

--- http/server.js
+++ http/server.js
@@ -2,10 +2,18 @@ import http from "http";
 
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-  res.write("hello");
-  res.end();
+  // res.write("hello");
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      status: "true",
+      message: "success",
+      data: { name: "ehtsham ul haq" },
+    }),
+  );
 });
 
 server.listen(PORT, () => {
-  console.log("server running : " + PORT);
+  console.log("server running at port: " + PORT);
 });
