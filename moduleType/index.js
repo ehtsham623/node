ab00b5e5d767336b9df03d4170da4909dcf1f05f
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-18 02:07:16 +0500

    added module type

---
+++ moduleType/index.js
@@ -0,0 +1,3 @@
+import { generateRandomNums, cToF } from "./utils.js";
+console.log(generateRandomNums());
+console.log(cToF(0));
---
+++ moduleType/utils.js
@@ -0,0 +1,7 @@
+export function generateRandomNums() {
+  return Math.floor(Math.random() * 100);
+}
+
+export function cToF(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
