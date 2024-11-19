330844ab704c3ebcff5ff81c725a5b75bdf67fe3
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-11-20 04:23:07 +0500

    req

--- http/server.js
+++ http/server.js
@@ -2,14 +2,25 @@ import http from "http";
 
 const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
-  // res.write("hello");
-
-  // console.log(req);
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello from server");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>defult page</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>about page</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>not found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/text" });
+    res.end("server error");
+  }
 });
 
 server.listen(PORT, () => {
